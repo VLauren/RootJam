a347078196e35a3e2685acf10b097950af752d17
RosaNarMu <rosanmn.n@gmail.com>
2023-02-03 21:16:02 +0100

    ejejejej z limitado

--- Assets/Scripts/RJChar.cs
+++ Assets/Scripts/RJChar.cs
@@ -6,6 +6,12 @@ public class RJChar : MonoBehaviour
 {
     CharacterController CharacterController;
 
+    int velocity = 8;
+
+    float minZMovement = -5;
+
+    float maxZMovement = 5;
+
     void Start()
     {
         CharacterController = GetComponent<CharacterController>();
@@ -14,11 +20,21 @@ public class RJChar : MonoBehaviour
     void Update()
     {
         // Muevo el personaje
-        Vector3 horizontalMovement = transform.right * Time.deltaTime * 8 * Input.GetAxisRaw("Horizontal");
-        Vector3 verticalMovement = transform.forward * Time.deltaTime * 8 * Input.GetAxisRaw("Vertical");
+        Vector3 horizontalMovement = transform.right * Time.deltaTime * velocity * Input.GetAxisRaw("Horizontal");
+        Vector3 verticalMovement = transform.forward * Time.deltaTime * velocity * Input.GetAxisRaw("Vertical");
         CharacterController.Move(horizontalMovement + verticalMovement);
 
-        // Cambio la rotación del personaje
+        // Cambio la rotaciï¿½n del personaje
         transform.rotation = Quaternion.Euler(Mathf.Atan2(transform.position.z, transform.position.y) * Mathf.Rad2Deg, 0, -(Mathf.Atan2(transform.position.x, transform.position.y) * Mathf.Rad2Deg));
+
+        if (transform.position.z <= minZMovement)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+        }
+
+        if (transform.position.z >= maxZMovement)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 5);
+        }
     }
 }
