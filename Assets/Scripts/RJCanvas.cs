6e88b51cc71426e78224ef63620dbc9f8f298559
RosaNarMu <rosanmn.n@gmail.com>
2023-02-04 20:15:02 +0100

    smashon smashof ahora bonito

---
+++ Assets/Scripts/RJCanvas.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RJCanvas : MonoBehaviour
+{
+
+    public GameObject smashSliderFill;
+    public GameObject smashSliderFrame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        smashSliderFill = transform.Find("SliderMachaca").gameObject;
+        smashSliderFrame = transform.Find("PresionaBoton").gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (RJChar.canGather)
+        {
+            smashSliderFill.SetActive(true);
+            smashSliderFrame.SetActive(true);
+        }
+        else
+        {
+            smashSliderFill.SetActive(false);
+            smashSliderFrame.SetActive(false);
+        }
+    }
+}
